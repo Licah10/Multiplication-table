2e24f1d4053de1598f26978f32742c30126d77c2
Licah10 <73362623+Licah10@users.noreply.github.com>
2021-03-12 11:19:32 +0000

    Add files via upload

---
+++ multiplication table.py
@@ -0,0 +1,38 @@
+print('--'*25)
+print('This will help you with your multiplication table!')
+print('--'*25)
+n = int(input('Chose a number between 1 and 100: '))
+if n >= 0 and n <= 100:
+    print('{} x 1 ='.format(n), n * 1)
+    print('{} x 2 ='.format(n), n * 2)
+    print('{} x 3 ='.format(n), n * 3)
+    print('{} x 4 ='.format(n), n * 4)
+    print('{} x 5 ='.format(n), n * 5)
+    print('{} x 6 ='.format(n), n * 6)
+    print('{} x 7 ='.format(n), n * 7)
+    print('{} x 8 ='.format(n), n * 8)
+    print('{} x 9 ='.format(n), n * 9)
+    print('{} x 10 ='.format(n), n * 10)
+    print('--' * 25)
+    print('I hope my multiplication table was useful for you!')
+    print('--' * 25)
+else:
+    n = int(input('ERROR!!! Chose a number between 1 and 100: '))
+
+print('{} x 1 ='.format(n), n * 1)
+print('{} x 2 ='.format(n), n * 2)
+print('{} x 3 ='.format(n), n * 3)
+print('{} x 4 ='.format(n), n * 4)
+print('{} x 5 ='.format(n), n * 5)
+print('{} x 6 ='.format(n), n * 6)
+print('{} x 7 ='.format(n), n * 7)
+print('{} x 8 ='.format(n), n * 8)
+print('{} x 9 ='.format(n), n * 9)
+print('{} x 10 ='.format(n), n * 10)
+print('--' * 25)
+print('I hope my multiplication table was useful for you!')
+print('--' * 25)
+
+
+
+
